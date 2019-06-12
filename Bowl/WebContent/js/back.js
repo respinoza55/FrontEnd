cc99d295d9306dea73a7983f8d38d88fa5ef6434
Dell <Dell@192.168.86.125>
2019-06-12 11:25:51 -0700

    updated javascript changes

--- Bowl/WebContent/js/back.js
+++ Bowl/WebContent/js/back.js
@@ -10,81 +10,141 @@ function myFirst(evt) {
 function calculate(form) {
 //	myFirst(evt);
 
-	// var first = parseInt(document.getElementById("first1").value);
-	// var second = parseInt(document.getElementById("second1").value);
-	var totalScore = 0;
-	var strike = false;
-	var strikeTwo = false;
-	var spare = false;
-
-	for (var i = 0; i <= 20; i++) {
-		form.ball[i].value = "";
-	}
-
-	for (var j = 0; j <= 18; j+=2) {
-
-		var frameScore = 0;
-		var shouldDisplay = false;
-
-		if (form.ball[j].value.toLowerCase() == 'x') {
-
-			frameScore += 10;
-		}
-	}
+	var displayScore = 0;
+	var thirdThrow = 0;
+	var nextThrow = 0;
 
 	// if (document.getElementById("first1").value.localeCompare("X") == 0) {
 	// 	first = 10;
 
-	// 	if (strikeTwo) {
-	// 		totalScore += 30;
-	// 	}
-	// 	else if (strike) {
-	// 		strikeTwo = true;
-	// 	}
-	// 	// else {
 
-	// 	// }
+//	var firstF = parseInt(document.getElementsByName("ball")[0].value);
+//	var secondF = parseInt(document.getElementsByName("ball")[1].value);
+//	var example = document.forms.player
+//	var list = form.elements["ball"];
 
-	// 	document.getElementById("result").innerHTML = totalScore;
-	// } 
-	// // else if (first + second == 10) {
-	// // 	document.getElementById("second1").innerHTML = "/";
-	// // 	spare = true;
-	// // }
-	// else {
-	// 	document.getElementById("result").innerHTML = first + second;
-	// }
+//	document.getElementById("result").innerHTML = totalScore;
+
+
+	 for (var i=0; i <= 18; i+=2) {
+
+		 var showScore = false;
+		 var totalScore = 0;
+
+		 // Check for a strike
+		 if (form.ball[i].value.toLowerCase() == 'x') 
+{
+		 	totalScore += 10;
+
+		 	if (i < 16) {
+				if (form.ball[i+1].value != '')			// Should not be able to put a value in the 
+					form.ball[i+1].value = '';			// 2nd input of a frame when a strike is thrown
+				nextThrow = form.ball[i+2].value;
+
+				if (nextThrow.toLowerCase() == 'x')
+					thirdThrow = form.ball[i+4].value;
+				else
+					thirdThrow = form.ball[i+3].value;
+		 	}
+		 	if (i == 16) {
+		 		nextThrow = form.ball[i+2].value;
+		 		thirdThrow = form.ball[i+3].value;
+		 	}
+		 	if (i == 18) {
+		 		nextThrow = form.ball[i+1].value;
+		 		thirdThrow = form.ball[i+2].value;
+		 	}
+		 	if (nextThrow != '' && thirdThrow != '') {
+		 		if (nextThrow.toLowerCase() == 'x') {
+		 			totalScore += 10;
+		 			if(thirdThrow.toLowerCase() == 'x')
+		 				totalScore += 10;
+		 			else
+		 				totalScore += parseInt(thirdThrow);
+
+		 		}
+ 		 		else {	// the 2nd throw is not a strike
+		 			if (thirdThrow == '/') {
+		 				totalScore += 10;
+		 			}
+		 			else {	// we have an open frame
+		 				totalScore += parseInt(nextThrow);
+		 				totalScore += parseInt(thirdThrow);
+		 			}
+ 		 		}
+		 		showScore = true;
+		 	}
+
+		 } 
+		 // Else we don't have a strike
+		 else if (form.ball[i].value != '' && form.ball[i+1].value != '') {
+
+		 	if (form.ball[i+1].value == '/') {
+
+		 		totalScore += 10;
+		 		if (form.ball[i+2].value != '') {
+
+		 			if (form.ball[i+2].value.toLowerCase() == 'x') {
+		 				totalScore += 10;
+		 			}
+		 			else {
+		 				totalScore += parseInt(form.ball[i+2].value);
+		 			}
+		 			showScore = true;
+		 		}
+		 	}/*What happens if the second throw is set to 'X', what do we do?
+		 		*/
+		 	else {
+		 		totalScore += parseInt(form.ball[i].value);
+		 		if (form.ball[i+1].value.toLowerCase() != 'x') {
+		 			totalScore += parseInt(form.ball[i+1].value);
+		 			showScore = true;
+		 		}
+		 		else
+		 			form.ball[i+1].value = '';
+		 	}
+		 }
+
+		 displayScore += totalScore;
+
+		 if(showScore) {
+		 	var k = i/2;
+		 	form.result[k].value = displayScore;
+		 }
+	 }
 }
 
-function calculate2(evt) {
-	myFirst(evt);
+function notes() {
 
-	var first = 0;
-	var totalScore = 0;
-	var strike = false;
-	var strikeTwo = false;
+	 if (form.ball[0].value == 3) {
+	 	form.result[0].value = form.ball[0].value;
+	 	totalScore += 3;
+	 }
 
-	if (document.getElementById("first2").value.localeCompare("X") == 0) {
-		first = 10;
 
-		if (strikeTwo) {
-			totalScore += 30;
-		}
-		else if (strike) {
-			strikeTwo = true;
-		}
-		// else {
+	 if (form.ball[2].value.toLowerCase() == 'x') {
+	 	form.result[1] = 'X';
+	 }
 
-		// }
+	 if (form.ball[2].value == 7) {
+	 	form.result[1].value = 'X';
+	 	totalScore += 7;
+	 } else if (form.ball[2].value == 'x') {
+	 	form.result[1] = 'X';
+	 	totalScore += 10;
+	 	strike = true;
+	 }
+		 for (var i = 0; i <= len; i++) {
 
-		document.getElementById("result2").innerHTML = totalScore;
-	} else {
+	 	//  if (form.ball[0].value == 'x') {
+		 // 	form.result[0].value = 'X';
+		 // 	form.ball[1].value = "";
+		 // }
 
-		first = parseInt(document.getElementById("first2").value);
-		var second = parseInt(document.getElementById("second2").value);
-
-		document.getElementById("result2").innerHTML = first + second;
-	}
+		 if (form.ball[i].value.toLowerCase() == 'x') {
+		 	form.result[i].value = '9';
+		 }
+	 }
 }
 
 
